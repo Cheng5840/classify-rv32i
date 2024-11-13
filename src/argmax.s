94241fa3ff78b545322adc384500d92429797093
Cheng5840 <daniel16448@gmail.com>
2024-11-13 14:20:00 +0800

    argmax successed !!

--- src/argmax.s
+++ src/argmax.s
@@ -25,13 +25,37 @@ argmax:
     li t6, 1
     blt a1, t6, handle_error
 
-    lw t0, 0(a0)
+    lw t0, 0(a0)            # t0 stores max value
 
-    li t1, 0
-    li t2, 1
+    li t1, 0                # t1 stores the idx of max value
+    li t2, 1                # current index
+
+    
+
+    
 loop_start:
     # TODO: Add your own implementation
+    bge t2, a1, end_loop    
+
+    slli t3, t2, 2
+    add t3, t3, a0
+    lw t4, 0(t3)            # t4 : current value
+    
+    bgt t4, t0, update_max
+    j next_element          
+
+update_max:
+    mv t0, t4              
+    mv t1, t2               
+
+next_element:
+    addi t2, t2, 1 
+    j loop_start            
+
+end_loop:
+    mv a0, t1               
+    jr ra                  
 
 handle_error:
-    li a0, 36
+    li a0, 36               
     j exit
